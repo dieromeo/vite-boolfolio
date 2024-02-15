fda22ee0f6a5d2ed4104495b9460d591134a0edc
dieromeo <diegoromeo99@icloud.com>
2024-02-15 16:05:31 +0100

    feat: navbar

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -6,6 +6,18 @@ export default {
 
 <template>
     <header>
-        <h1>Header</h1>
+        <!-- navbar -->
+        <nav class="bg-dark text-white border-bottom border-body">
+            <div class="container d-flex justify-content-between align-items-center ">
+                <h1 class="py-3 m-0">Progetti</h1>
+                <ul class="d-flex m-0">
+                    <li class="list-unstyled"><a class="text-decoration-none me-3 text-white" href="#">link</a></li>
+                    <li class="list-unstyled"><a class="text-decoration-none me-3 text-white" href="#">link</a></li>
+                    <li class="list-unstyled"><a class="text-decoration-none me-3 text-white" href="#">link</a></li>
+                    <li class="list-unstyled"><a class="text-decoration-none me-3 text-white" href="#">link</a></li>
+                </ul>
+            </div>
+
+        </nav>
     </header>
 </template>
