f0dfcf32d9ddd7957ca43ecc36db43b0017ac2bb
dieromeo <diegoromeo99@icloud.com>
2024-02-16 17:38:38 +0100

    feat: header link component

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,6 +1,9 @@
 <script>
+import HeaderLInks from './HeaderLInks.vue';
+
 export default {
-    name: 'headerComponent'
+    name: 'headerComponent',
+    components: { HeaderLInks }
 }
 </script>
 
@@ -10,22 +13,9 @@ export default {
         <nav class="bg-dark text-white border-bottom border-body">
             <div class="container d-flex justify-content-between align-items-center ">
                 <h1 class="py-3 m-0">Progetti</h1>
-                <ul class="d-flex m-0">
-                    <li class="list-unstyled"><router-link :to="{ name: 'home' }"
-                            class="text-decoration-none text-white ms-3">Home</router-link></li>
-                    <li class="list-unstyled"><router-link :to="{ name: 'projects' }"
-                            class="text-decoration-none text-white ms-3">Progetti</router-link></li>
-                    <li class="list-unstyled"><router-link :to="{ name: 'about' }"
-                            class="text-decoration-none text-white ms-3">About</router-link></li>
-                </ul>
+                <HeaderLInks />
             </div>
 
         </nav>
     </header>
 </template>
-
-<style lang="scss" scoped>
-header ul li:hover {
-    text-decoration: underline;
-}
-</style>
---
+++ src/components/header/HeaderLInks.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'HeaderLinks',
+    data() {
+        return {
+            links: ['Home', 'Progetti', 'About']
+        }
+    }
+}
+</script>
+
+<template>
+    <ul class="d-flex m-0">
+        <li v-for="link in links" class="list-unstyled"><router-link :to="{ name: link.toLowerCase() }"
+                class="text-decoration-none text-white ms-3">{{ link }}</router-link>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+li:hover {
+    text-decoration: underline;
+}
+</style>
